3b2a7a2dd832289ba8a02559f1130bdf78c242f0
FAWZIA NUR <FAWZIANUR@GMAIL.com>
2022-01-16 21:23:00 -0500

    day 1 challenge 2

--- validate_subseq.js
+++ validate_subseq.js
@@ -45,3 +45,56 @@ function isValidSubsequence(array, sequence) {
   
       }
   }
+
+
+
+
+//   option 2
+
+function isValidSubsequence(array, sequence) {
+  // Write your code here.
+	
+	let aIndex = 0;
+	let sIndex = 0;
+	
+// 	while the array index is LESS than the array length and
+// 	the seq index is less than the seq length
+	while (aIndex < array.length && sIndex < sequence.length){
+// 		counter of the index for both array and seq will go up if there is a match
+		// console.log(array[aIndex], "seq", sequence[sIndex])
+		if (array[aIndex] === sequence[sIndex])
+			sIndex++;
+			aIndex++;
+			// console.log(sIndex)
+	}
+	// 		after the entire loop is searched 
+			return sIndex === sequence.length
+
+}
+
+
+// option 3
+
+function isValidSubsequence(array, sequence) {
+    // Write your code here.
+      
+  // 	set counter for seqIndex
+      let seqInd = 0;
+  // 	for of loop to traverse numbers in the arrat and compare to seq
+      for (const num of array){
+  // 		if the seqInd matched the sequence array length - than the for loop
+  // 		can end bc it matches!
+          if (seqInd === sequence.length) {
+              break;
+          }
+  // 		otherwise if the lengths dont match than if the sequence # matches the 
+  // 		number in the array than the index counter should increase
+          else if (sequence[seqInd] === num){
+              seqInd++
+          }
+  // 		if neither match the loop will continue but the counter just wont be increased
+  // 		but the next NUM will be looped through
+      }
+      return seqInd === sequence.length
+      
+  }
